ce10d4450de50d73cda6da0c8adb76d5aeaaa50a
“Davit <kobauri.dato@gmail.com>
2023-02-06 21:55:38 +0400

    Commit

--- wdio.conf.js
+++ wdio.conf.js
@@ -134,8 +134,12 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
-
+    reporters: [
+        'spec',
+        ['junit', {
+            outputDir: './report'
+        }]
+    ],
 
     
     //
